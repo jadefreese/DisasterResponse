5adbc7abdc39146b8b3b03599cd4acc4d31a709b
Jessica DeFreese <jadefreese98@gmail.com>
2021-09-08 17:47:09 -0500

    fill in code on template

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,88 @@
 import sys
-
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    
+    '''
+    FUNCTION: 
+        Load csv files containing the messages and categories
+
+    INPUTS:
+        messages_filepath - csv file containing messages
+        categories_filepath - csv file containing categories
+
+    OUTPUTS
+        df - data frame of the merged data sets of messages and categories
+
+    '''
+
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, how='outer', on='id')
+    
+    return df
 
 
 def clean_data(df):
-    pass
 
+    '''
+    FUNCTION: 
+        clean the data in the give dataframe to remove duplicates
+
+    INPUTS:
+        df - data frame imported from the load_data function
+
+    OUTPUTS
+        df - data frame of the cleaned input data frame
+
+    '''
+
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+
+    # use the first row of the categories dataframe to extract a list of new 
+    # column names for categories
+    row = categories.iloc[0]
+    category_colnames = list(map(lambda x: x[:-2] , row))
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    # convert category values to just numbers 0 or 1
+    for column in categories:
+        categories[column] = categories[column].str[-1:]
+        categories[column] = categories[column].astype(int)
+    
+
+    # replace categories column in df with new category columns
+    df = df.drop(['categories'], axis=1)
+    df = pd.concat([df, categories], axis=1)
+
+    # remove duplicates
+    df.related.replace(2,1,inplace=True)
+    df.drop_duplicates('message', inplace=True)
+    df.reset_index()
+
+    return df
 
 def save_data(df, database_filename):
-    pass  
+
+    '''
+    FUNCTION: 
+        save the data fram as an SQL Database
+
+    INPUTS:
+        df - data frame of cleaned data
+        database_filename - the file name/location of where to save the database
+
+    OUTPUTS
+        SQL Database
+    '''
+    
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql(database_filename, engine, index=False, if_exists='replace')  
 
 
 def main():
